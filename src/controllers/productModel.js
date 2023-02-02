130d9c488c56747dc9eb360d48012ba391d45204
amankr1635 <amankr.1635@gmail.com>
2023-02-02 17:23:21 +0530

    product model

---
+++ src/controllers/productModel.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+      //  valid number/decimal
+    },
+    currencyId: {
+      type: String,
+      required: true,
+      default: "INR"
+    },
+    currencyFormat: {
+      type: String,
+      required: true,
+      default: "₹"
+    },
+    isFreeShipping: {
+      type: Boolean,
+      default: false
+    },
+    productImage: {
+      type: String,
+      required: true
+    }, // s3 link
+    style: String,
+    availableSizes: {
+      enum: ["S", "XS", "M", "X", "L", "XXL", "XL"]
+    },
+    installments: Number,
+    deletedAt: Date,
+    isDeleted: {
+      type: Boolean,
+      default: false
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("product", productSchema);
