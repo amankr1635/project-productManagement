47db2482454d4506caaac80b2b13dae9115c0d8c
amankr1635 <amankr.1635@gmail.com>
2023-02-02 17:48:37 +0530

    getUsers

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const productModel = require('../models/productModel')
 
 const product = async function (req, res) {
@@ -10,4 +11,75 @@ const product = async function (req, res) {
     });
 }
 
-module.exports = {product}
+
+
+
+
+
+
+// Returns product details by product id
+// - __Response format__
+//   - _**On success**_ - Return HTTP status 200. Also return the product documents. The response should be a JSON object like [this](#successful-response-structure)
+//   - _**On error**_ - Return a suitable error message with a valid HTTP status code. The response should be a JSON object like [this](#error-response-structure)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const getProduct = async function(req,res){
+    let productId = req.params.productId
+    if(mongoose.isValidObjectId(productId)) return res.status(400).send({status:false , message : "Please ented a valid productId"})
+    
+let products = await productModel.findOne({_id: productId, isDeleted: false})
+if(!products){
+    return res.status(404).send({status: false, message : "No product available on given userId"})
+}
+return res.status(200).send({status: true, message: "Success", data: products})
+}
+
+module.exports = {product,getProduct}
--- src/router.js
+++ src/router.js
@@ -1,11 +1,7 @@
 const express = require("express");
-const {
-  user,
-  login,
-  getUser,
-  updateUser,
-} = require("./controllers/userController");
 const router = express.Router();
+
+const {user,login,getUser,updateUser} = require("./controllers/userController");
 const { authentication, authorization } = require("./middleware/middleware");
 
 router.get("/test-me", function (req, res) {
@@ -16,5 +12,8 @@ router.post("/register", user);
 router.post("/login", login);
 router.get("/user/:userId/profile", authentication, getUser);
 router.put("/user/:userId/profile", updateUser);
+
+
+
 
 module.exports = router;
