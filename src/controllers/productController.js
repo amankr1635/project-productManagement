49d1d7b938f5edcb7aa3c6f9ddde66d5d127fa75
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-02-02 17:47:29 +0530

    get data

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -11,3 +11,57 @@ const product = async function (req, res) {
 }
 
 module.exports = {product}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const getProduct=async function(req,res)
+{
+    try{
+
+    let data=req.query;
+    if(Object.keys(data).length==0)
+    {
+    let data=await productModel.find({isDeleted:false})
+    
+
+    return res.status(200).send({status:true,message:"",data:data})
+
+    }
+    else{
+        let product=await productModel.find({isDeleted:false,...data})
+        if(product.length==0) return res.status(404).send({status:false,message:"no prodect found"})
+        return res.status(200).send({status:true,data:product})
+
+    }
+}
+catch(error){
+   return res.status(500).send({ status: false, msg: err.message });
+}
+
+}
