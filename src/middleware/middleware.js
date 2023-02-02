8598aac93ecb71b12c52f88a8d02ae9468f7f60d
amankr1635 <amankr.1635@gmail.com>
2023-02-02 12:27:15 +0530

    middlewale

---
+++ src/middleware/middleware.js
@@ -0,0 +1,49 @@
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const userModel = require("../models/userModel");
+
+const authentication = async function(req,res){
+let token = req.auth
+let userId = req.params.userId
+// if(!userId) return res.status(400).send({status: false, message: "Please enter userId"})
+if(!mongoose.Types.ObjectId.isValid(userId))return res.status(400).send({status: false, message: "enter a valid userId"})
+// console.log(req.authorization)
+if(!token) return res.status(400).send({status: false, message: "please enter token"})
+jwt.verify(token, "group5californium", (err, decodedToken) => {
+    if (err) {
+      return res.status(400).send({ status: false, message: "invalid token" });
+    } else {
+      req.decodedToken = decodedToken;
+      next();
+    }
+})
+}
+
+const authorization = async function(req,res){
+    let userToken = req.decodedToken.userId;
+  let userId = req.params.userId;
+  if (!mongoose.Types.ObjectId.isValid(userId))
+    return res.status(400).send({ status: false, message: "Invalid user ID" });
+
+  let userData = await userModel.findOne({ _id: userId });
+  if (!userData)
+    return res
+      .status(404)
+      .send({ status: false, message: "user with this ID is not present." });
+
+  if (userToken != userData.userId)
+    return res
+      .status(401)
+      .send({ status: false, message: "You are not authorized" });
+  if (userData.isDeleted == true) {
+    return res
+      .status(404)
+      .send({ status: false, message: "document already deleted" });
+  }
+
+  next();
+};
+
+
+
+module.exports = {authentication,authorization}
--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { user, login } = require("./controllers/userController");
 const router = express.Router()
-
+const {authentication,authorization} = require("./middleware/middleware")
 
 
 router.get('/test-me',function(req,res){
+    // console.log(req.authorization.)
     res.send({test:"test-api"})
 })
 
