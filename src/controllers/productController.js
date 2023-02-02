a5a5a8cbe09e6f5bb67e49ab30730edf4d0c0199
vpalve07 <vinayak.palve02@gmail.com>
2023-02-02 17:31:01 +0530

    productStart-commit

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,2 +1,13 @@
 const productModel = require('../models/productModel')
 
+const product = async function (req, res) {
+    let data = req.body
+    let createProduct = await productModel.create(data)
+    return res.status(201).send({
+        status: true,
+        message: "User created successfully",
+        data: createProduct,
+    });
+}
+
+module.exports = {product}
