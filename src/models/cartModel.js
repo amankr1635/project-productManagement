04dbc882273d878c0b46e7c02b73a3fc4d295fb3
amankr1635 <amankr.1635@gmail.com>
2023-02-06 13:32:34 +0530

    Cart Model

---
+++ src/models/cartModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+let ObjectId = mongoose.Schema.Types.ObjectId
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: ObjectId,
+      ref: "usersdata",
+      required: true
+    },
+    items: {
+      type: [Object],
+      productId: {
+        type: ObjectId,
+        ref: "product",
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true
+      }
+    },
+    totalPrice: {
+      type: Number,
+      required: true
+    },
+    totalItems: {
+      type: Number,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("cart", cartSchema);
